0cc4fff4cdd855567ce8396aa93ee9635325cc3a
CiprianBunescu <ciprian.bunescu@gmail.com>
2026-05-11 21:40:14 +0300

    Oferte - stil super profi

--- main.js
+++ main.js
@@ -57,9 +57,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const initSwiper = () => {
         new Swiper('.promo-swiper', {
-            effect: 'cards',
+            effect: 'coverflow',
             grabCursor: true,
-            initialSlide: 0,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: true,
+            },
             loop: true,
             autoplay: {
                 delay: 3000,
